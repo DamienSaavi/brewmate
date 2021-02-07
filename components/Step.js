945e4c8824099c57527d169ba3d567df92407123
Damien Saavi <damiensaavi@gmail.com>
2021-02-07 12:48:35 -0800

    added editing step locally

--- components/Step.js
+++ components/Step.js
@@ -1,39 +1,48 @@
-import React, { Component, useEffect } from 'react'
-import { StyleSheet, Text, View, TextInput } from 'react-native'
+import React, { Component, useState } from 'react'
+import { Text, View, TextInput, TouchableWithoutFeedback } from 'react-native'
 import { Card, ListItem, Image, Button, Icon } from 'react-native-elements'
+import { State, PanGestureHandler } from 'react-native-gesture-handler';
 
 export default function Step(props) {
+    const [editable, setEditable] = useState(false)
+    const [description, setDescription] = useState(props.step.description)
+    const [ingredient, setIngredient] = useState(props.step.ingredient)
+    const [timer, setTimer] = useState(props.step.timer)
+
     return (
-        <View style={props.style.container}>
-            <View style={props.style.descing}>
-                {props.step.editable ? (
-                    <TextInput
-                        style={props.style.description}
-                        defaultValue={props.step.description}
-                        onEndEditing={(value) => {props.step.description=value}}
-                    />
-                ):(
-                    <Text style={props.style.description}>{props.step.description}</Text>
-                )}
-                {props.step.ingredient ?
-                    (<View style={props.style.ingredients}>
-                        {props.step.ingredient.map((ingredient, index) => {
-                            return (
-                                <Text style={{ color: 'white', paddingHorizontal: 12 }} key={index}>{ingredient.name + '  ' + String(ingredient.amount) + ingredient.unit}</Text>
-                            )
-                        })}
-                    </View>) : (null)}
+        <TouchableWithoutFeedback
+            onLongPress={() => { setEditable(!editable) }}>
+            <View style={props.style.container}>
+                <View style={props.style.descing}>
+                    {editable ? (
+                        <TextInput
+                            style={props.style.description}
+                            defaultValue={description}
+                            onChangeText={(text) => setDescription(text)}
+                        />
+                    ) : (
+                            <Text style={props.style.description}>{description}</Text>
+                        )}
+                    {ingredient ?
+                        (<View style={props.style.ingredients}>
+                            {ingredient.map((ingredient, index) => {
+                                return (
+                                    <Text style={{ color: 'white', paddingHorizontal: 12 }} key={index}>{ingredient.name + '  ' + String(ingredient.amount) + ingredient.unit}</Text>
+                                )
+                            })}
+                        </View>) : (null)}
+                </View>
+                {timer ?
+                    <View style={props.style.timer}>
+                        <Text>{timer.name}</Text>
+                        <Icon
+                            name='timer'
+                            type='Ionicons'
+                            color='#000'
+                        />
+                        <Text>{String(timer.time.minute).padStart(2, '0') + ':' + String(timer.time.second).padStart(2, '0')}</Text>
+                    </View> : null}
             </View>
-            {props.step.timer ?
-                <View style={props.style.timer}>
-                    <Text>{props.step.timer.name}</Text>
-                    <Icon
-                        name='timer'
-                        type='Ionicons'
-                        color='#000'
-                    />
-                    <Text>{String(props.step.timer.time.minute).padStart(2,'0') + ':' + String(props.step.timer.time.second).padStart(2,'0')}</Text>
-                </View> : null}
-        </View>
+        </TouchableWithoutFeedback>
     )
 }
